8e1fa8e74cd8e1ea6ad2ce964781add26ca03297
Wartz <wartzay@gmail.com>
2014-10-31 00:41:03 -0400

    adding lemonade class

---
+++ src/square/Lemonade.java
@@ -0,0 +1,32 @@
+package square;
+
+public class Lemonade 
+	{
+	private int cups;
+	private int size;
+	private double priceTotal;
+	
+	public Lemonade(int cupCount, int cupSize) 
+	{
+		cups = cupCount;
+		size = cupSize;
+	}
+	
+	private double price()
+	{
+		double small = 1.25;
+		double medium = 1.75;
+		double large = 1.95;
+				
+		if (size == 3) 
+		{
+			priceTotal = cups*large;
+		}
+		else if (size == 2)
+		{
+			priceTotal = cups*medium;
+		}
+		else priceTotal = cups*small;
+		return priceTotal;
+	}
+}
